2fe381226667cffa17866e6ae2f233ccde4db80c
Supakorn Intaratat <supakorn.intaratat@pranworks.com>
2019-07-12 15:25:28 +0700

    update project

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once 'process.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>    
@@ -13,23 +15,71 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <?php require_once 'process.php' ?>
-    <div class="row justify-content-center">
-        <form action="process.php" method="POST">
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" name="name" class="form-control" value="Enter your name">
-            </div>
+    <?php if(isset($_SESSION['message'])): ?>
+        <div class="alert alert-<?= $_SESSION['msg_type']; ?>">
+        <?php 
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        ?>
+        </div>        
+    <?php endif; ?>
+    <div class="container">
+        <?php
+            $result = $conn->query("SELECT * FROM data");
 
-            <div class="form-group">
-                <label for="location">Location</label>
-                <input type="text" name="location" class="form-control" value="Enter your location">
-            </div>
+            //pre_r($result);            
+            function pre_r($array){
+                echo '<pre>';
+                print_r($array);
+                echo '</pre>';
+            }
+        ?>
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+            <?php while($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['location']; ?></td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['id'] ?>" class="btn btn-info">Edit</a>
+                        <a href="process.php?delete=<?php echo $row['id'] ?>" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
 
-            <div class="form-group">        
-                <button class="btn btn-primary" type="submit" name="save">Save</button>
-            </div>
-        </form>
-    </div>    
+            </table>
+        </div>
+
+        <div class="row justify-content-center">
+            <form action="process.php" method="POST">
+                <input type="hidden" name="id" value="<?= $id; ?>">
+                
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="form-control" value="<?=$name;?>" placeholder="Enter your name">
+                </div>
+
+                <div class="form-group">
+                    <label for="location">Location</label>
+                    <input type="text" name="location" class="form-control" value="<?=$location;?>" placeholder="Enter your location">
+                </div>
+
+                <div class="form-group">     
+                    <?php if($update == true): ?>   
+                        <button class="btn btn-primary" type="submit" name="update">Update</button>
+                    <?php else: ?>
+                        <button class="btn btn-primary" type="submit" name="save">Save</button>
+                    <?php endif; ?>
+                </div>
+            </form>
+        </div>    
+    </div>
 </body>
 </html>
--- process.php
+++ process.php
@@ -1,5 +1,58 @@
 <?php
+session_start();
+$name = "";
+$location = "";
+$update = false;
+$id = 0;
 
-require 'config/constants.php';
+require 'config/db.php';
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(mysqli_error($conn));
+if(isset($_POST['save'])){
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+    $conn->query("INSERT INTO data (name, location) VALUES('$name','$location')") or die($conn->error);
+
+    $_SESSION['message'] = "Record has been saved!";
+    $_SESSION['msg_type'] = "success";
+    
+    header('location: index.php');
+    exit();
+}
+
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $conn->query("DELETE FROM data WHERE id=$id") or die($conn->error());
+
+    $_SESSION['message'] = "Record has been deleted!";
+    $_SESSION['msg_type'] = "danger";
+    
+    header('location: index.php');
+    exit();
+}
+
+if(isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $conn->query("SELECT * FROM data WHERE id=$id") or die($conn->error());
+    
+    if($result->num_rows == 1 ){
+        $row = $result->fetch_assoc();
+        $name = $row['name'];
+        $location = $row['location'];
+    }
+}
+
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $conn->query("UPDATE data SET name='$name', location='$location' WHERE id=$id") or die($conn->error());
+    
+    $_SESSION['message'] = "Record has been updated!";
+    $_SESSION['msg_type'] = "warning";
+    
+    header('location: index.php');
+    exit();
+}
